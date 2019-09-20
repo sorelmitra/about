7916bd6799c27aa713d17ac2898ab536db566e75
Sorel Mitra <sorelmitra@yahoo.com>
2019-09-20 11:18:17 +0300

    Deploy

--- script.js
+++ script.js
@@ -193,49 +193,6 @@ function applyLanguage(currentLang) {
 	}
 }
 
-function setupModal(modalParentId, modalOpenButtonId) {
-	let modalRootId = "id-modal-root";
-	let modalCloseButtonId = "id-modal-close-button";
-	createModalElements(modalParentId, modalRootId, modalCloseButtonId, "modal-test");
-	let createdButton = false;
-	if (modalOpenButtonId == null) {
-		modalOpenButtonId = "id-test-modal-open-button";
-		$(`#${modalParentId}`).append(
-			$('<button/>', {
-				id: modalOpenButtonId,
-			}).text("Open Modal"),
-		);
-		createdButton = true;
-	}
-
-	// Get the modal
-	var modal = document.getElementById(modalRootId);
-	// Get the button that opens the modal
-	var btn = document.getElementById(modalOpenButtonId);
-	// Get the <span> element that closes the modal
-	var span = document.getElementById(modalCloseButtonId);
-
-	// When the user clicks the button, open the modal 
-	if (createdButton) {
-		btn.onclick = function() {
-			parseAndRenderMarkDown();
-			modal.style.display = "block";
-		}
-
-		// When the user clicks on <span> (x), close the modal
-		span.onclick = function() {
-			modal.style.display = "none";
-		}
-
-		// When the user clicks anywhere outside of the modal, close it
-		window.onclick = function(event) {
-			if (event.target == modal) {
-				modal.style.display = "none";
-			}
-		}
-	}
-}
-
 function createModalElements(modalParentId, modalRootId, modalCloseButtonId, modalPartId) {
 	$(`#${modalParentId}`).empty();
 	$(`#${modalParentId}`).append(
