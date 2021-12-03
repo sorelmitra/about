dd8e2ef809ebd422579e23a188f4d42365c64dd8
Sorel Mitra <>
2021-12-03 12:27:25 +0200

    Deploy

--- script.js
+++ script.js
@@ -68,10 +68,6 @@ const texts = {
 		'RO': 'TIMP LIBER',
 		'EN': 'FREE TIME'
 	},
-	'photo-albums': {
-		'RO': 'ALBUME FOTO',
-		'EN': 'PHOTO ALBUMS'
-	},
 	'contact': {
 		'RO': 'CONTACT',
 		'EN': 'CONTACT'
@@ -96,12 +92,10 @@ const texts = {
 
 const knownSubpages = {
 	"freetime": ["sailing", "black-fortress", "toy-bridge", "toy-yacht"],
-	"photo-albums": ["USA-2007"]
 };
 
 const subPageNavStyles = {
 	"freetime": "subpage-nav-innerblock",
-	"photo-albums": "subpage-nav-innerblock2"
 }
 
 
@@ -519,14 +513,6 @@ function createMenuItems(modalPartId) {
 				onclick: 'gotoPage("freetime/index.html")',
 				class: 'menu-item-link'
 			}).html(getText('freetime')),
-		),
-		$('<div/>', {
-			class: 'menu-item'
-		}).append(
-			$('<a/>', {
-				onclick: 'gotoPage("photo-albums/index.html")',
-				class: 'menu-item-link'
-			}).html(getText('photo-albums')),
 		),
 		$('<div/>', {
 			class: 'menu-item'
