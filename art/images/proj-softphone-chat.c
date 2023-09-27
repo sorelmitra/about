3447ea4aad1fb89c394660849d45a7433ceb7a23
Sorel Mitra <sorelmitra@yahoo.com>
2023-09-27 20:13:58 +0300

    Deploy

---
+++ art/images/proj-softphone-chat.c
@@ -0,0 +1,20 @@
+#include <learn.h>
+#include <develop.h>
+
+int softphone_and_chatapp() {
+	while (1) {
+		learn_some("Objective C");
+		learn_some("Mac OS Cocoa");
+		develop_some("Softphone Features);
+		if (switch_to_chatapp()) break;
+	}
+	while () {
+        learn_some("Angular JS");
+        develop_some("Chat App Features);
+        if (switch_to_softphone()) break;
+	}
+	while (1) {
+        develop_some("Softphone Features);
+	}
+}
+
